21103ee2c13ed392d478fb12bfb288820d0a8d08
csprun <97461577+csprun@users.noreply.github.com>
2022-03-10 19:33:00 -0500

    now working code

--- src/models/LSTM_classifier.py
+++ src/models/LSTM_classifier.py
@@ -10,60 +10,29 @@ class TextLSTM(nn.Module):
         super(TextLSTM, self).__init__()
 
         self.params = params
-        self.batch_size = self.params['batch_size']
-        self.output_size = self.params['num_classes']
-        self.hidden_size = self.params['hidden_size']
-        self.embedding_length = self.params['emb_dim']
-        self.vocab_size = vocab_size
-        self.embedding = nn.Embedding(vocab_size, self.params['emb_dim'])
-        self.embedding.weight = nn.Parameter(word_embeddings, requires_grad=False)
+        
+        self.embeddings = nn.Embedding(vocab_size, self.params['emb_dim'])
+        self.embeddings.weight = nn.Parameter(word_embeddings, requires_grad=False)
+        
+        self.lstm = nn.LSTM(input_size=self.params['emb_dim'],
+                            hidden_size=self.params['hidden_size'],
+                            num_layers=self.params['hidden_layers'],
+                            dropout=self.params['dropout'],
+                            bidirectional=self.params['is_bidirectional'])
 
-        self.dropout = self.params['dropout']
-        
-        self.lstm = nn.LSTM(self.embedding_length, self.hidden_size, num_layers=1, batch_first=True)
-        self.fc = nn.Linear(self.hidden_size, self.output_size)
-        self.act = nn.Sigmoid()
-        
-        
-    def forward(self, input_sentence):
-        """ 
-        Parameters
-        ----------
-        input_sentence: input_sentence of shape = (batch_size, num_sequences)
-        batch_size : default = None. Used only for prediction on a single sentence after training (batch_size = 1)
-        
-        Returns
-        -------
-        Output of the linear layer containing logits for positive & negative class which receives its input as the final_hidden_state of the LSTM
-        final_output.shape = (batch_size, output_size)
-        
-        """
-        
-        ''' Here we will map all the indexes present in the input sequence to the corresponding word vector using our pre-trained word_embedddins.'''
-        input = self.embedding(input_sentence) # embedded input of shape = (batch_size, num_sequences,  embedding_length)
-        # input = input #.permute(1, 0, 2) # input.size() = (num_sequences, batch_size, embedding_length)
-        # print(input.size())
-        # if batch_size is None:
-        #    h_0 = Variable(torch.zeros(1, self.batch_size, self.hidden_size)) # Initial hidden state of the LSTM
-        #    c_0 = Variable(torch.zeros(1, self.batch_size, self.hidden_size)) # Initial cell state of the LSTM
-        #else:
-        #    h_0 = Variable(torch.zeros(1, batch_size, self.hidden_size))
-        #    c_0 = Variable(torch.zeros(1, batch_size, self.hidden_size))
-        cur_batch_size = input.size()[1]
-        input = input.permute(1, 0, 2)
-        # lens = torch.tensor([embeds.size()[0],embeds.size()[1],embeds.size()[2]], device='cpu')
-        h_0 = Variable(torch.zeros(1, cur_batch_size, self.hidden_size))
-        c_0 = Variable(torch.zeros(1, cur_batch_size, self.hidden_size))
-        #packed_embedded = nn.utils.rnn.pack_padded_sequence(embeds, lens)
-        # packed_outputs, (hidden,cell) = self.lstm(input)
-        # hidden = torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim = 1)
-        # dense_outputs = self.fc(hidden)
-        #outputs=self.act(dense_outputs)
-        output, (final_hidden_state, final_cell_state) = self.lstm(input, (h_0, c_0))
-        # final_hidden_state = self.dropout(final_hidden_state)
-        # hidden = torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim = 1)
-        final_output = self.fc(final_hidden_state[-1]) # final_hidden_state.size() = (1, batch_size, hidden_size) & final_output.size() = (batch_size, output_size)
-        # final_output = self.sigmoid(final_output)
+        self.dropout = nn.Dropout(self.params['dropout'])
 
-        return final_output
+        self.fc = nn.Linear(self.params['hidden_size'] * self.params['hidden_layers']* (1+self.params['is_bidirectional']),
+                            self.params['num_classes'])
 
+        
+    def forward(self, x):
+       embedded_sent = self.embeddings(x)
+
+       lstm_out, (h_n,c_n) = self.lstm(embedded_sent)
+       final_feature_map = self.dropout(h_n)
+
+       final_feature_map = torch.cat([final_feature_map[i,:,:] for i in range(final_feature_map.shape[0])], dim=1)
+       final_out = self.fc(final_feature_map)
+
+       return final_out
--- src/train_lstm.py
+++ src/train_lstm.py
@@ -14,14 +14,20 @@ def main():
         'num_classes': 4,
         'epochs': 15,
         'lr': 0.0001,
-        'batch_size': 32,
+        'batch_size': 64,
         'dropout': 0.05,
-        'hidden_size': 256
+        'hidden_size': 8,
+        'hidden_layers': 2,
+        'is_bidirectional': True
     }
 
     ag_news = Dataset()
     model = TextLSTM(word_embeddings=ag_news.pretrained_embeds,
                     vocab_size=ag_news.vocab_size, params=params)
+
+    ## TO LOAD OLD MODEL
+    # model.load_state_dict(torch.load(PATH))
+    # model.eval()
 
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.AdamW(model.parameters(), lr=params['lr'])
@@ -42,6 +48,10 @@ def main():
                               shuffle=True, collate_fn=generate_batch)
     test_loader = DataLoader(ag_news.test_data, batch_size=params['batch_size'],
                              shuffle=True, collate_fn=generate_batch)
+
+    epoch_avg_train_loss = []
+    epoch_avg_valid_loss = []
+
     print('begin training')
     for epoch in range(1, params['epochs'] + 1):
         epoch_start_time = time.time()
@@ -49,6 +59,14 @@ def main():
         total_acc, total_count = 0, 0
         log_interval = 100
         start_time = time.time()
+
+        epoch_train_loss = 0.0
+        epoch_train_count = 0.0
+
+        ## SAVE MODEL STATE AND LOSS HISTORY
+        torch.save(model.state_dict(), 'TextLSTM_state.pt')
+        torch.save(epoch_avg_train_loss, 'LSTM_epoch_avg_train_loss.pt')
+        torch.save(epoch_avg_valid_loss, 'LSTM_epoch_avg_valid_loss.pt')
 
         for idx, (label, text) in enumerate(train_loader):
             optimizer.zero_grad()
@@ -59,6 +77,10 @@ def main():
             optimizer.step()
             total_acc += (predicted_label.argmax(1) == label).sum().item()
             total_count += label.size(0)
+
+            epoch_train_loss += loss
+            epoch_train_count += 1
+
             if idx % log_interval == 0 and idx > 0:
                 elapsed = time.time() - start_time
                 print('| epoch {:3d} | {:5d}/{:5d} batches '
@@ -70,13 +92,22 @@ def main():
         model.eval()
         total_acc, total_count = 0, 0
 
+        epoch_avg_train_loss.append(epoch_train_loss / epoch_train_count)
+
+        epoch_valid_loss = 0.0
+        epoch_valid_count = 0.0
+
         with torch.no_grad():
             for idx, (label, text) in enumerate(valid_loader):
                 predicted_label = F.softmax(model(text), dim=1)
                 loss = criterion(predicted_label, label)
                 total_acc += (predicted_label.argmax(1) == label).sum().item()
                 total_count += label.size(0)
+                epoch_valid_loss += loss 
+                epoch_valid_count += 1
         accu_val = total_acc/total_count
+
+        epoch_avg_valid_loss.append(epoch_valid_loss / epoch_valid_count)
 
         print('-' * 69)
         print('| end of epoch {:3d} | time: {:5.2f}s | '
@@ -84,6 +115,7 @@ def main():
                                                time.time() - epoch_start_time,
                                                accu_val))
         print('-' * 69)
+
 
     print('Training Complete!')
     print('Checking the results of test dataset.')
@@ -98,6 +130,21 @@ def main():
             total_count += label.size(0)
     accu_test = total_acc/total_count
     print('test accuracy {:8.3f}'.format(accu_test))
+
+    ## SAVE MODEL STATE AND LOSS HISTORY
+    torch.save(model.state_dict(), 'TextLSTM_state.pt')
+    torch.save(epoch_avg_train_loss, 'LSTM_epoch_avg_train_loss.pt')
+    torch.save(epoch_avg_valid_loss, 'LSTM_epoch_avg_valid_loss.pt')
+
+    ## PLOT TRAIN/VALID LOSS OVER TIME
+    fig, ax = plt.subplots(figsize=(9,5))
+    ax.plot(epoch_avg_train_loss)
+    ax.plot(epoch_avg_valid_loss)
+    # ax.set_title('LSTM Classifier')
+    ax.set_xlabel('Epoch')
+    ax.set_ylabel('Loss')
+    ax.legend(['training loss','validation loss'])
+    fig.savefig('LSTM_loss_graph.png')
 
 
 if __name__ == '__main__':
